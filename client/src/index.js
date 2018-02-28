b7bd75aa970d5cfa6c18b892ca80fc22377c282e
Aaron Middleton <aarontm9@gmail.com>
2018-02-28 10:49:47 -0700

    react build

--- client/src/index.js
+++ client/src/index.js
@@ -17,6 +17,3 @@ ReactDOM.render(
       <Routes />
     </BrowserRouter>
   </Provider>, document.getElementById('root'));
-
-//Client ID 872b1d94c72549d385e12be6ef820b73
-//Client Secret f74f01b1e6b3478ca6c2ffe1c8fc4544
